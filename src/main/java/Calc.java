1941689bfd122a67f77600bc86c83efdcece0752
HP <g00433839@atu.ie>
2024-10-03 11:40:09 +0100

    Added code for user to enter Second number

---
+++ src/main/java/Calc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        System.out.println("Please enter in the first number: ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println("Please enter in the second number: ");
+        int b = input.nextInt();
+
+
+
+    }
+}
